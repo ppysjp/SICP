e0cf40e08a73885878349df23e80defa44d206d7
Samuel Palmer <Samuel.Palmer@dpr.co.uk>
2022-07-11 12:56:51 +0100

    Code for the introcution of assignment

--- Chapter3/CostOfAssignment.rkt
+++ Chapter3/CostOfAssignment.rkt
@@ -1,1 +1,24 @@
 #lang racket
+
+(define (make-simplified-withdraw balance)
+  (lambda (amount)
+    (begin (set! balance (- balance amount))
+	   balance)))
+
+
+(define W (make-simplified-withdraw 25))
+
+(W 20)
+
+(W 10)
+
+
+(define (make-decrementer balance)
+  (lambda (amount)
+    (- balance amount)))
+
+(define D (make-decrementer 25))
+
+(D 20)
+
+(D 10)
