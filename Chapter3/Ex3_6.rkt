33a7c516c940bbd0f8e3bb94f59881c2b7157c82
Samuel Palmer <Samuel.Palmer@dpr.co.uk>
2022-07-11 12:00:06 +0100

    Have a random number generator object that does stuff based on arguments of reset and generate

--- Chapter3/Ex3_6.rkt
+++ Chapter3/Ex3_6.rkt
@@ -1,26 +1,35 @@
 #lang racket
 
-(require "Ex3_5.rkt")
-
-
 ; I'm going to define a new rand that
 ;will solve will be the solution to this problem.
 
+(define (dispatch m)
+      (cond ((eq? m 'reset) (print "RESET"))
+	    ((eq? m 'generate) (print "GENERATE"))
+	    (else (error "Unknown request --RANDOM-NUMBER-GENERATOR" m))))
+
+
+(define (rand-update x)
+  (let ((a 75)
+	(b 74)
+	(m (+ 1 (expt 2 16))))
+    (modulo (+ (* a x) b) m)))
+
 (define (r)
-  (let ((seed 0)
-	(define (dispatch m)
-	  (cond ((eq? m 'reset) (lambda (x (set! seed x))))
-		((eq? m 'generate) (begin (set! seed (rand-update seed))
-					  seed))
-		(else error "invalid operation")))
-	dispatch)))
+  (define (dispatch m)
+      (cond ((eq? m 'reset) (print "RESET"))
+	    ((eq? m 'generate) (print "GENERATE"))
+	    (else (error "Unknown request --RANDOM-NUMBER-GENERATOR" m))))
+  dispatch)
+
 
 (define rand (r))
 
+(rand 'reset)
 (rand 'generate)
-(rand 'generate)
-(rand 'generate)
-(rand 'generate)
+
+
+
 
 
 
