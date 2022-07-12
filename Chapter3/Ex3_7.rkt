2ff9bee59f6eb3413185dca0a341fe47a69c9e3a
Samuel Palmer <Samuel.Palmer@dpr.co.uk>
2022-07-12 17:41:59 +0100

    Created extended password checker for joint account

--- Chapter3/Ex3_7.rkt
+++ Chapter3/Ex3_7.rkt
@@ -12,3 +12,14 @@
 
 ;; (define (make-joint account password new-password)
 ;;   )
+
+(define (correct-password? passwords m)
+  (cond ((null? passwords) false)
+	((eq? (car passwords) m) true)
+	(else (correct-password? (cdr passwords) m))))
+
+(define passwords (list 'open-sesame 'rose-bud))
+
+(correct-password? passwords 'hello)
+(correct-password? passwords 'open-sesame)
+(correct-password? passwords 'rose-bud)
