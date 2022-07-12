4255e77d165b47e885bd53b545b24e8c8a651354
Samuel Palmer <Samuel.Palmer@dpr.co.uk>
2022-07-12 13:24:21 +0100

    Imperative pitfalls

---
+++ Chapter3/PitfallsImperative.rkt
@@ -0,0 +1,27 @@
+#lang racket
+
+;; functional implementation
+
+;; (define (factorial n)
+;;   (define (iter product counter)
+;;     (if (> counter n)
+;; 	product
+;; 	(iter (* counter product)
+;; 	      (+ counter 1))))
+;;   (iter 1 1))
+
+;; imperative implementation
+
+(define (factorial n)
+  (let ((product 1)
+	(counter 1))
+    (define (iter)
+      (if (> counter n)
+	  product
+	  (begin (set! product (* counter product))
+		 (set! counter (+ counter 1))
+		 (iter))))
+    (iter)))
+
+
+(factorial 3)
