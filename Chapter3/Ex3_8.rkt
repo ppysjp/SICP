aaf1e067efd56bfcc22dda7dd58166a32e3bdebd
Samuel Palmer <Samuel.Palmer@dpr.co.uk>
2022-07-15 17:48:16 +0100

    Introduce lambda and redefine f

--- Chapter3/Ex3_8.rkt
+++ Chapter3/Ex3_8.rkt
@@ -13,13 +13,18 @@
 (define (f x)
   (let ((a 0))
     (if (= x 1)
-      0
-      0)))
+	(begin (set! a (+ a 1))
+	       0)
+      a)))
+
+
+(define f1 (f 1))
+f1
 
 (define f0 (f 0))
 f0
-(define f1 (f 1))
-f1
+
+
 
 
 
