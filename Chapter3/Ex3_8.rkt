671111e396991ebfc2a12453d6e9310473946d36
Samuel Palmer <Samuel.Palmer@dpr.co.uk>
2022-07-15 17:58:47 +0100

    The internal state variable a isn't keeping its state which is annoying

--- Chapter3/Ex3_8.rkt
+++ Chapter3/Ex3_8.rkt
@@ -6,23 +6,29 @@
 ;;     (begin (set! a (+ a 1))
 ;; 	   (- a x))))
 
+;; (define (f x)
+;;   (let ((a 0))
+;;     (if (= x 1)
+;; 	(begin (set! a (+ a 1))
+;; 	       0)
+;;       a)))
 
+(define (g)
+  (lambda (x)
+    (let ((a 0))
+      (display "The value of a: ")
+      (display a)
+      (display "\n")
+      (if (= x 1)
+	  (begin (set! a (+ a 1))
+		 0)
+          a))))
 
+(define f (g))
 
-
-(define (f x)
-  (let ((a 0))
-    (if (= x 1)
-	(begin (set! a (+ a 1))
-	       0)
-      a)))
-
-
-(define f1 (f 1))
-f1
-
-(define f0 (f 0))
-f0
+(f 1)
+(f 1)
+(f 1)
 
 
 
