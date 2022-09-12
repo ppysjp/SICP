54dcfc7f193056760005f00edad53812de4e521c
Samuel Palmer <Samuel.Palmer@dpr.co.uk>
2022-09-12 20:51:39 +0100

    Just created the new file for my box and ponter diagram for the cycle...

--- Chapter3/Ex3_13.rkt
+++ Chapter3/Ex3_13.rkt
@@ -6,3 +6,17 @@
 	(if (null? (mcdr x))
 		x
 		(last-pair (mcdr x))))
+
+(define (append! x y)
+	(set-mcdr! (last-pair x) y)
+	x)
+
+(define (make-cycle x)
+	(set-mcdr! (last-pair x) x)
+	x)
+
+; What is the box pointer that is formed from this mutable list to create z?
+
+(define z (make-cycle (mlist 'a 'b 'c)))
+
+z
