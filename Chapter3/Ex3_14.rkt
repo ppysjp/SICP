7669f2efb9513d40481926a72437de4cd5975d8f
Samuel Palmer <Samuel.Palmer@dpr.co.uk>
2022-09-13 13:56:29 +0100

    Finish typing up the latest version for .txt and .rkt files

--- Chapter3/Ex3_14.rkt
+++ Chapter3/Ex3_14.rkt
@@ -1,2 +1,20 @@
 #lang racket
 (require compatibility/mlist)
+
+(define (mystery x)
+	(define (loop x y)
+		(if (null? x)
+				y
+				(let ((temp (mcdr x)))
+					(set-mcdr! x y)
+				 	(loop temp x))))
+	(loop x '()))
+
+(define v (mlist 'a 'b 'c 'd))
+
+v
+
+(define w (mystery v))
+
+w
+
